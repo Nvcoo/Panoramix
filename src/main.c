2abefb70f2edad5753922009597a39724cddeef1
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-05-04 17:35:44 +0200

    add: usage implementation

--- src/main.c
+++ src/main.c
@@ -5,7 +5,6 @@
 ** main
 */
 
-#include <stdio.h>
 #include <stdlib.h>
 #include "panoramix.h"
 
@@ -14,15 +13,13 @@ int main(int ac, char **av)
     int value = 0;
 
     if (ac != 5) {
-        printf("USAGE: ./panoramix <nb_villagers> <pot_size> <nb_fights> <nb_refills>\n");
-        printf("Values must be >0.\n");
+        print_usage();
         return 84;
     }
     for (int i = 1; i < ac; i++) {
         value = atoi(av[i]);
         if (value <= 0) {
-            printf("USAGE: ./panoramix <nb_villagers> <pot_size> <nb_fights> <nb_refills>\n");
-            printf("Values must be >0.\n");
+            print_usage();
             return 84;
         }
     }

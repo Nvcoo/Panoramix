1587a81d667d786337f8c90ea470c6e42cad19f5
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-05-05 00:34:45 +0200

    fix: usage message being too long

--- src/print_usage.c
+++ src/print_usage.c
@@ -10,6 +10,7 @@
 
 void print_usage(void)
 {
-    printf("USAGE: ./panoramix <nb_villagers> <pot_size> <nb_fights> <nb_refills>\n");
+    printf("USAGE: ./panoramix <nb_villagers> <pot_size> ");
+    printf("<nb_fights> <nb_refills>\n");
     printf("Values must be >0.\n");
 }

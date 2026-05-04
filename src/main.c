4c8d07573349793295b76b9c0d79cfc8178e69af
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-05-04 17:31:36 +0200

    add: usage message

--- src/main.c
+++ src/main.c
@@ -5,3 +5,26 @@
 ** main
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "panoramix.h"
+
+int main(int ac, char **av)
+{
+    int value = 0;
+
+    if (ac != 5) {
+        printf("USAGE: ./panoramix <nb_villagers> <pot_size> <nb_fights> <nb_refills>\n");
+        printf("Values must be >0.\n");
+        return 84;
+    }
+    for (int i = 1; i < ac; i++) {
+        value = atoi(av[i]);
+        if (value <= 0) {
+            printf("USAGE: ./panoramix <nb_villagers> <pot_size> <nb_fights> <nb_refills>\n");
+            printf("Values must be >0.\n");
+            return 84;
+        }
+    }
+    return 0;
+}

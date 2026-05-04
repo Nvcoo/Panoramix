74d481d2211cc879c83b37b3ce174a3d4a41b3a6
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-05-04 17:35:23 +0200

    add: usage printing file

---
+++ src/print_usage.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2026
+** Panoramix
+** File description:
+** print_usage
+*/
+
+#include "panoramix.h"
+#include <stdio.h>
+
+void print_usage(void)
+{
+    printf("USAGE: ./panoramix <nb_villagers> <pot_size> <nb_fights> <nb_refills>\n");
+    printf("Values must be >0.\n");
+}

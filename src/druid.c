6eb0a6cb0b507dcf809da0ca6414ed80cf172d30
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-05-04 19:22:47 +0200

    add: druid thread, mutex, and cond loop

--- src/druid.c
+++ src/druid.c
@@ -6,11 +6,20 @@
 */
 
 #include "panoramix.h"
-#include <stdio.h>
 
 void *druid_routine(void *arg)
 {
-    (void)arg;
+    pot_t *pot = (pot_t *)arg;
+
     printf("Druid: I'm ready... but sleepy...\n");
+    while (pot->pot_refills > 0) {
+        pthread_mutex_lock(&pot->mutex);
+        pthread_cond_wait(&pot->wake_druid, &pot->mutex);
+        pot->pot_refills--;
+        pot->portion = pot->pot_size;
+        printf("Druid: Ah! Yes, yes, I'm awake! Working on it! Beware I can only make %d more refills after this one.\n", pot->pot_refills);
+        pthread_cond_broadcast(&pot->pot_refilled);
+        pthread_mutex_unlock(&pot->mutex);
+    }
     return NULL;
 }

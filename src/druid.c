08fb9dccf57e4eeb2b2973d87be6525f6d4455fc
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-05-04 17:59:25 +0200

    add: druid thread and thread creation

--- src/druid.c
+++ src/druid.c
@@ -5,3 +5,12 @@
 ** druid
 */
 
+#include "panoramix.h"
+#include <stdio.h>
+
+void *druid_routine(void *arg)
+{
+    (void)arg;
+    printf("Druid thread started\n");
+    return NULL;
+}
--- src/main.c
+++ src/main.c
@@ -5,14 +5,17 @@
 ** main
 */
 
-#include <stdio.h>
+#include <pthread.h>
 #include <stdlib.h>
 #include "panoramix.h"
 
 int main(int ac, char **av)
 {
     int value = 0;
+    int nb_villagers = 0;
+    int nb_fights = 0;
     pot_t pot;
+    pthread_t druid_thread;
 
     if (ac != 5) {
         print_usage();
@@ -28,5 +31,20 @@ int main(int ac, char **av)
     pot.pot_size = atoi(av[2]);
     pot.portion = pot.pot_size;
     pot.pot_refills = atoi(av[4]);
+    nb_villagers = atoi(av[1]);
+    nb_fights = atoi(av[3]);
+    pthread_t villager_threads[nb_villagers];
+    villager_t villagers[nb_villagers];
+    for (int i = 0; i < nb_villagers; i++) {
+        villagers[i].id = i;
+        villagers[i].nb_fights = nb_fights;
+        villagers[i].pot = &pot;
+    }
+    for (int i = 0; i < nb_villagers; i++)
+        pthread_create(&villager_threads[i], NULL, villager_routine, &villagers[i]);
+    pthread_create(&druid_thread, NULL, druid_routine, &pot);
+    for (int i = 0; i < nb_villagers; i++)
+        pthread_join(villager_threads[i], NULL);
+    pthread_join(druid_thread, NULL);
     return 0;
 }

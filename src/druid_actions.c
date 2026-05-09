ffe962875f8ff4ea4280274bf6ae7b144253a090
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-05-09 16:16:56 +0200

    fix: druid_actions file now matches struct organization

--- src/druid_actions.c
+++ src/druid_actions.c
@@ -9,13 +9,13 @@
 
 void do_refill(pot_t *pot)
 {
-    pot->pot_refills--;
-    pot->portion = pot->pot_size;
+    pot->data.pot_refills--;
+    pot->data.portion = pot->data.pot_size;
     printf("Druid: Ah! Yes, yes, I'm awake! Working on it! ");
     printf("Beware I can only make %d more refills after this one.\n",
-        pot->pot_refills);
-    pot->druid_ready = false;
-    pthread_cond_broadcast(&pot->pot_refilled);
-    pot->druid_ready = true;
-    pthread_cond_broadcast(&pot->pot_refilled);
+        pot->data.pot_refills);
+    pot->flags.druid_ready = false;
+    pthread_cond_broadcast(&pot->sync.pot_refilled);
+    pot->flags.druid_ready = true;
+    pthread_cond_broadcast(&pot->sync.pot_refilled);
 }

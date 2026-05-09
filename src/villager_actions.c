e79cea6347e2a30c28b76cde0ef1ec72019e2b4a
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-05-09 16:22:03 +0200

    fix: villager_actions file now matches struct organization

--- src/villager_actions.c
+++ src/villager_actions.c
@@ -9,25 +9,25 @@
 
 void wait_for_refill(villager_t *vill)
 {
-    while (vill->pot->portion == 0 && !vill->pot->druid_done) {
+    while (vill->pot->data.portion == 0 && !vill->pot->flags.druid_done) {
         printf("Villager %d: Hey Pano wake up! We need more potion.\n",
             vill->id);
-        while (!vill->pot->druid_ready)
-            pthread_cond_wait(&vill->pot->pot_refilled, &vill->pot->mutex);
-        pthread_cond_signal(&vill->pot->wake_druid);
-        pthread_cond_wait(&vill->pot->pot_refilled, &vill->pot->mutex);
+        while (!vill->pot->flags.druid_ready)
+            pthread_cond_wait(&vill->pot->sync.pot_refilled, &vill->pot->sync.mutex);
+        pthread_cond_signal(&vill->pot->sync.wake_druid);
+        pthread_cond_wait(&vill->pot->sync.pot_refilled, &vill->pot->sync.mutex);
     }
 }
 
 int do_fight(villager_t *villager)
 {
-    villager->pot->portion--;
-    pthread_mutex_unlock(&villager->pot->mutex);
-    sem_get(&villager->pot->sem);
+    villager->pot->data.portion--;
+    pthread_mutex_unlock(&villager->pot->sync.mutex);
+    sem_get(&villager->pot->sync.sem);
     villager->nb_fights--;
     printf("Villager %d: Take that roman scum! Only %d left.\n",
         villager->id, villager->nb_fights);
-    sem_release(&villager->pot->sem);
+    sem_release(&villager->pot->sync.sem);
     if (villager->nb_fights == 0) {
         printf("Villager %d: I'm going to sleep now.\n", villager->id);
         return 1;

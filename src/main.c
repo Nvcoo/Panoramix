5e742ddf142c193b384458b9b61c1dfa64635928
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-05-09 15:58:52 +0200

    fix: all coding styles left

--- src/main.c
+++ src/main.c
@@ -27,6 +27,19 @@ void pot_destroy(pot_t *pot)
     pthread_cond_destroy(&pot->pot_refilled);
 }
 
+void start_villagers(pot_t *pot, pthread_t *threads, villager_t *vill,
+    int nb_vill)
+{
+    for (int i = 0; i < nb_vill; i++)
+        pthread_create(&threads[i], NULL, villager_routine, &vill[i]);
+    for (int i = 0; i < nb_vill; i++)
+        pthread_join(threads[i], NULL);
+    pthread_mutex_lock(&pot->mutex);
+    pot->villagers_done = true;
+    pthread_cond_signal(&pot->wake_druid);
+    pthread_mutex_unlock(&pot->mutex);
+}
+
 void simulate(pot_t *pot, char **av)
 {
     int nb_villagers = atoi(av[1]);
@@ -46,14 +59,7 @@ void simulate(pot_t *pot, char **av)
     while (!pot->druid_ready)
         pthread_cond_wait(&pot->pot_refilled, &pot->mutex);
     pthread_mutex_unlock(&pot->mutex);
-    for (int i = 0; i < nb_villagers; i++)
-        pthread_create(&villager_threads[i], NULL, villager_routine, &villagers[i]);
-    for (int i = 0; i < nb_villagers; i++)
-        pthread_join(villager_threads[i], NULL);
-    pthread_mutex_lock(&pot->mutex);
-    pot->villagers_done = true;
-    pthread_cond_signal(&pot->wake_druid);
-    pthread_mutex_unlock(&pot->mutex);
+    start_villagers(pot, villager_threads, villagers, nb_villagers);
     pthread_join(druid_thread, NULL);
 }
 

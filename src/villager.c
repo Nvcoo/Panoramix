b90b2890c413e43f200eeb845d8a77ec40a035e5
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-05-09 15:09:30 +0200

    fix: villagers coding styles

--- src/villager.c
+++ src/villager.c
@@ -20,6 +20,22 @@ void wait_for_refill(villager_t *vill)
     }
 }
 
+int do_fight(villager_t *villager)
+{
+    villager->pot->portion--;
+    pthread_mutex_unlock(&villager->pot->mutex);
+    sem_get(&villager->pot->sem);
+    villager->nb_fights--;
+    printf("Villager %d: Take that roman scum! Only %d left.\n",
+        villager->id, villager->nb_fights);
+    sem_release(&villager->pot->sem);
+    if (villager->nb_fights == 0) {
+        printf("Villager %d: I'm going to sleep now.\n", villager->id);
+        return 1;
+    }
+    return 0;
+}
+
 void *villager_routine(void *arg)
 {
     villager_t *villager = (villager_t *)arg;
@@ -35,17 +51,8 @@ void *villager_routine(void *arg)
             printf("Villager %d: I'm going to sleep now.\n", villager->id);
             return NULL;
         }
-        villager->pot->portion--;
-        pthread_mutex_unlock(&villager->pot->mutex);
-        sem_get(&villager->pot->sem);
-        villager->nb_fights--;
-        printf("Villager %d: Take that roman scum! Only %d left.\n",
-            villager->id, villager->nb_fights);
-        sem_release(&villager->pot->sem);
-        if (villager->nb_fights == 0) {
-            printf("Villager %d: I'm going to sleep now.\n", villager->id);
+        if (do_fight(villager))
             return NULL;
-        }
     }
     return NULL;
 }

27f7527347953d35bd0160539e2b33a9403c16d0
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-05-09 16:18:09 +0200

    fix: druid file now matches struct organization

--- src/druid.c
+++ src/druid.c
@@ -12,18 +12,18 @@ void *druid_routine(void *arg)
     pot_t *pot = (pot_t *)arg;
 
     printf("Druid: I'm ready... but sleepy...\n");
-    pthread_mutex_lock(&pot->mutex);
-    pot->druid_ready = true;
-    pthread_cond_broadcast(&pot->pot_refilled);
-    while (pot->pot_refills > 0) {
-        pthread_cond_wait(&pot->wake_druid, &pot->mutex);
-        if (pot->villagers_done)
+    pthread_mutex_lock(&pot->sync.mutex);
+    pot->flags.druid_ready = true;
+    pthread_cond_broadcast(&pot->sync.pot_refilled);
+    while (pot->data.pot_refills > 0) {
+        pthread_cond_wait(&pot->sync.wake_druid, &pot->sync.mutex);
+        if (pot->flags.villagers_done)
             break;
         do_refill(pot);
     }
-    pot->druid_done = true;
-    pthread_cond_broadcast(&pot->pot_refilled);
-    pthread_mutex_unlock(&pot->mutex);
+    pot->flags.druid_done = true;
+    pthread_cond_broadcast(&pot->sync.pot_refilled);
+    pthread_mutex_unlock(&pot->sync.mutex);
     printf("Druid: I'm out of viscum. I'm going back to... zZz\n");
     return NULL;
 }

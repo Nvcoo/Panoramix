892707b57724a3054d06e105965d231881f8da0d
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-05-12 18:32:00 +0200

    add: sem setup & cleanup

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 */
 
 #include "panoramix.h"
+#include <semaphore.h>
 
 int main(int ac, char **av)
 {
@@ -26,6 +27,7 @@ int main(int ac, char **av)
     pot_init(&pot, av);
     simulate(&pot, av);
     pot_destroy(&pot);
-    sem_cleanup(&pot.sync.sem);
+    //sem_cleanup(&pot.sync.sem);
+    sem_destroy(&pot.sync.sem);
     return 0;
 }
--- src/simulation.c
+++ src/simulation.c
@@ -6,6 +6,7 @@
 */
 
 #include "panoramix.h"
+#include <semaphore.h>
 
 void start_villagers(pot_t *pot, pthread_t *threads, villager_t *vill,
     int nb_vill)
@@ -28,7 +29,8 @@ void simulate(pot_t *pot, char **av)
     villager_t villagers[nb_villagers];
     pthread_t druid_thread;
 
-    sem_setup(&pot->sync.sem, 1);
+    //sem_setup(&pot->sync.sem, 1);
+    sem_init(&pot->sync.sem, 0, 1);
     for (int i = 0; i < nb_villagers; i++) {
         villagers[i].id = i;
         villagers[i].nb_fights = nb_fights;
